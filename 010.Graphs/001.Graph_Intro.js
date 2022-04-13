f940a94c3e57df4c97cc19ac4b94e473ab724063
Dileep <98578797+iamdileep89@users.noreply.github.com>
2022-04-13 19:21:03 +0000

    more

--- 010.Graphs/001.Graph_Intro.js
+++ 010.Graphs/001.Graph_Intro.js
@@ -14,7 +14,13 @@ WHAT ARE GRAPHS ?
 It is a data structure consists of a finite set of vertices or nodes or points, together with a set of unordered pairs of these vertices for an undirected graph or a set of ordered pairs for a directed graph
 NODES + CONNECTIONS == GRAPH
 
-, 
+           A
+         /   \
+        B     C
+        |     |
+        D --- E
+         \   /
+           F
 
 -----------------------------------------------------------------------------------------------
 USES OF GRAPHS ?
